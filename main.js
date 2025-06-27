6ca0b98166e1c9284d3b15674aaf663435bc6a91
jebersole13 <jcebersole1@gmail.com>
2025-06-27 21:46:23 +0900

    basic draft

--- main.js
+++
@@ -1,6 +0,0 @@
-const menuButton = document.getElementById("menu-button")
-const mobileDropDown= document.getAnimations("mobile-nav-dropdown")
-
-menuButton.addEventListener("click",()=>{
-    mobileDropDown.classList.toggle("hidden")
-}  )
